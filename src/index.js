2ae731aab3ea393f26fd2853ecfb802987f2af5c
Nguyen TienDung <dungnguyentien140602@gmail.com>
2022-08-11 22:48:29 +0700

    QueryParameter and Form default behavior

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const express = require('express')
 // Import template engine
 const xhbr = require('express-handlebars')
 
-const morgan = require('morgan')
+const morgan = require('morgan');
+const { log } = require('console');
 
 const app = express();
 
@@ -25,6 +26,19 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 console.log(path.join(__dirname, 'resources/views'));
 
+
+/**
+ * request : chứa tất cả thông tin liên quan đến yêu cầu gửi 
+ * response : dữ liệu được trả về , giúp tùy chọn cấu hình trả về 
+ */
+
+// Set up tuyến đường gửi đi qua 4 phương thức : get , post , put , delete
+
+// Mặc định trình duyệt là phương thức get
+// Post -> dùng JS hoặc submit Form 
+// Bấm thẻ a theo mặc định hoặc truy cập địa chỉ => get
+// Phân biệt dựa vào cái route 
+
 app.get('/', (req, res) => {
   // render thằng home.hbs với đường dẫn là '/'
     res.render('home');
@@ -32,10 +46,31 @@ app.get('/', (req, res) => {
 
 // Dữ liệu 2 thằng news.hbs và home.hbs được nạp vào file main.hbs trong folder layouts
 
+// req : là một object có các thuộc tính trong đó có query 
+// Truy cập vào nó bằng req.query
+
 app.get('/news', (req, res) => {
 
   // render thằng news.hbs với đường dẫn là '/news'
   res.render('news');
 });
+
+app.get('/search', (req, res) => {
+  
+  // Lấy ra thằng object query
+  console.log(req.query)
+
+  // Lấy ra thằng "q"
+  // console.log(req.query.q)
+  // render thằng news.hbs với đường dẫn là '/news'
+  res.render('search');
+});
+
+// Định nghĩa tuyến đường với phương thức post
+// Chạy vào đây khi submit form với method là "POST"
+app.post('/search' , (req, res) => {
+  res.render('search')
+})
+
 
 app.listen(port , () => console.log(`Example app listening : http://localhost:${port}`));
